4632d5a79edce437607ed6796e2b627d3ddf44ac
Jason Peak <jp5ive@gmail.com>
2022-03-17 22:15:23 -0700

    nit

---
+++ README.md
@@ -0,0 +1,87 @@
+## Heffalumps and Woozles
+Solutions for math challenge problems brought home from Ms. G's 3rd grade class.
+
+### Heffalump #3 - $1.00 Word Search
+
+#### Prompt
+Find as many words as possible that add up to $1.00, given that the letters in a word have value following the pattern 'a' = $0.01, 'b' = $0.02, ..., 'z' = $0.26.
+
+With values assigned to the letters in this pattern, words like "excellent" and "discipline" total $1.00.
+
+#### Solving with brute-force in python
+one-dollar-words.py is a command line Python3 script that can be run with various options to explore this prompt.
+
+##### Usage
+```
+$ ./one-dollar-words.py -h
+usage: one-dollar-words.py [-h] [--dictionary-file DICTIONARY_FILE] [--letter-score]
+                           [--score SCORE] [--skip-plurals] [--word WORD]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --dictionary-file DICTIONARY_FILE, -f DICTIONARY_FILE
+                        Path to a file
+  --letter-score, -l    whether to print individual letter scores
+  --score SCORE, -s SCORE
+                        show only words matching this target score
+  --skip-plurals, -p    whether to include plurals as valid
+  --word WORD, -w WORD  Score a single word
+```
+
+Two modes exist and other options can be set to modify the behavior:
+
+###### System dictionary
+Without arguments, the script reads its input from /usr/share/dict/words, a dictionary provided under Linux. Running the script with no options is equivalent to running it with `--dictionary-file /usr/share/dict/words`. On other systems, provide this option, specifying the full path to a text file of words, one per line.
+
+```
+$ ./one-dollar-words.py
+a: 1
+aachen: 32
+aaliyah: 57
+aardvark: 76
+aardvarks: 95
+aaron: 49
+...
+zwingli: 100
+zworykin: 141
+zygote: 98
+zygotes: 117
+zyrtec: 97
+zyuganov: 131
+```
+
+This output can be modified by providing any of the other options. For example, specifying `--score 101` will only emit words whose 'scores' equal 101.
+
+The `--letter-score` option causes the script to print the value of each letter, in addition to the total score for a word:
+
+It's possible to specify one or more words on the command line, causing the script to ignore the system (or user-specified) dictionary and evaluate only commandline input. Combining this option with `--letter-score` is useful for checking your math.
+
+Evaluate a word provided as an argument on the command line, for example:
+```
+$ ./one-dollar-words.py -w woozle
+woozle: 96
+```
+
+Multiple words and `--letter-score`:
+```
+$ ./one-dollar-words.py --letter-score -w heffalump -w woozle
+
+h:          8
+e:          5
+f:          6
+f:          6
+a:          1
+l:         12
+u:         21
+m:         13
+p:         16
+heffalump: 88
+
+w:      23
+o:      15
+o:      15
+z:      26
+l:      12
+e:       5
+woozle: 96
+```
---
+++ one-dollar-words.py
@@ -0,0 +1,100 @@
+#!/usr/bin/python3
+
+import argparse
+import re
+import string
+import sys
+
+
+def get_letter_map():
+    """Flip the list of ascii chars so that the index becomes the value"""
+    letter_map = {}
+    idx = 1
+    for letter in string.ascii_lowercase:
+        letter_map[letter] = idx
+        idx += 1
+    return letter_map
+
+def score_word(word, letter_map=None):
+    """Sum the values for each character according to letter_map"""
+    score = 0
+    word = word.lower()
+    if re.match(r'^[a-z]+$', word) is None:
+        raise Exception(f"'{word}' is not in [a-z]")
+
+    if letter_map is None:
+        letter_map = get_letter_map()
+
+    for character in word:
+        score += letter_map[character]
+
+    return score
+
+def get_default_word_list_from_file(dictionary_file):
+    """Param dictionary file should have one word per line
+
+       Words will be lowercased in the resultinglist.
+       'Words' containing non-letter characters will be dropped.
+    """
+    with open(dictionary_file, 'r') as f:
+        lines = sorted([line.strip().lower() for line in f.readlines()])
+        list_of_words = []
+        for word in lines:
+            if re.match(r'^[a-z]+$', word) is None:
+                continue
+            list_of_words.append(word)
+        return list_of_words
+
+def get_args_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dictionary-file", "-f", help="Path to a file", default="/usr/share/dict/words")
+    parser.add_argument("--letter-score", "-l", help="whether to print individual letter scores", action='store_true')
+    parser.add_argument("--score", "-s", help="show only words matching this target score", type=int)
+    parser.add_argument("--skip-plurals", "-p", help="whether to include plurals as valid", action='store_true', default=False)
+    parser.add_argument("--word", "-w", help="Score a single word", action='append')
+    return parser
+
+def word_score(word, score):
+    return f'{word}: {score}'
+    
+def per_letter_score(word, letter_map=None):
+    if letter_map is None:
+        letter_map = get_letter_map()
+    word_length = len(word)
+    total_score_width = len(str(score_word(word)))
+    padding = word_length + total_score_width
+    lines_to_print = []
+    for idx,char in enumerate(word):
+        line = f"{char}:{letter_map[char]: > {padding}}"
+        lines_to_print.append(line)
+    return lines_to_print
+        
+
+def remove_plurals(word_list):
+    singulars = []
+    for word in word_list:
+        if word.lower().endswith('s'):
+            continue
+        singulars.append(word)
+    return singulars
+
+if __name__ == '__main__':
+    parser = get_args_parser()
+    args = parser.parse_args()
+
+    if args.word:
+        word_list = args.word
+    else:
+        word_list = get_default_word_list_from_file(args.dictionary_file)
+
+    if args.skip_plurals is True:
+        word_list = remove_plurals(word_list)
+
+    for word in word_list:
+        score = score_word(word)
+        if args.score and score != args.score:
+            continue
+        if args.letter_score:
+            print()
+            print("\n".join(per_letter_score(word)))
+        print(word_score(word, score))
